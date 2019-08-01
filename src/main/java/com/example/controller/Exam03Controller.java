4626ab521f9f7dd44aff264fa17706037716026b
hiroki-okazaki <1031okazaki@gmail.com>
2019-08-01 17:41:29 +0900

    exam03完成

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,26 +1,30 @@
 package com.example.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/ex03")
 public class Exam03Controller {
 
+	@Autowired
+	private ServletContext application;
+
 	@RequestMapping("")
 	public String index() {
-		return "exam02";
+		return "exam03";
 	}
-	
+
 	@RequestMapping("calc-item")
-	public String calc(int num1, int num2 ,Model model) {
-		int answer = num1 * num2;
-		
-		model.addAttribute("num1",num1);
-		model.addAttribute("num2",num2);
-		model.addAttribute("answer",answer);
-		return "exam02-result";
+	public String calc(int num1, int num2, int num3) {
+		int answer = num1 + num2 + num3;
+		int taxanswer = (int) (answer * 1.08);
+		application.setAttribute("answer", answer);
+		application.setAttribute("taxanswer", taxanswer);
+		return "exam03-result";
 	}
-	
+
 }
